a4f73d919db74d540882ac6ea9fe4d55f6e20787
dgmaikon <dgmaikon@hotmail.com>
2023-01-05 15:23:13 -0300

    Change Imports and renders.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//default import
+import fname from './name.js';
+
+import pi, { triplePi, doublePi } from './math.js';
+
+//global import
 import * as calc from './calculator.js';
-import fname, {number} from './name.js';
 
 /* import {add, subtract, multiply, divide}; */ //import function by function
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const name = ReactDOM.createRoot(document.getElementById('name'));
+const names = ReactDOM.createRoot(document.getElementById('names'));
+const math = ReactDOM.createRoot(document.getElementById('math'));
 
 root.render(
   <ul>
-    <h2>Pi</h2>
+    <h2>Numbers</h2>
     <li>{calc.add(1, 2)}</li>
     <li>{calc.multiply(2, 3)}</li>
     <li>{calc.subtract(7, 2)}</li>
     <li>{calc.divide(5, 2)}</li>
-  </ul>
+  </ul>,
+  document.getElementById("root")
 );
 
-name.render(
+names.render(
   <div>
     <h1>Hello {fname}</h1>
-    <p>Your Luck number is {number}</p>
-  </div>,
-  document.getElementById("name")
+    <p>Your Luck number is {Math.floor(Math.random() * 10)}</p>
+  </div>
 );
+
+math.render(
+  <ul>
+ <h2>Pi</h2>
+  <li class="firstLi">{pi}</li>
+  <li class="secondLi">{doublePi()}</li>
+  <li class="thirdLi">{triplePi()}</li>
+ </ul>,
+ document.getElementById("math")
+ );
