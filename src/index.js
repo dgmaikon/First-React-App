49e8322c46b692475b4cd79bed5b73a30007cfd9
dgmaikon <dgmaikon@hotmail.com>
2023-01-05 14:35:27 -0300

    Add import, nameDOM and render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import * as calc from './calculator.js';
+import fname, {number} from './name.js';
 
 /* import {add, subtract, multiply, divide}; */ //import function by function
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = ReactDOM.createRoot(document.getElementById('name'));
 
 root.render(
   <ul>
+    <h2>Pi</h2>
     <li>{calc.add(1, 2)}</li>
     <li>{calc.multiply(2, 3)}</li>
     <li>{calc.subtract(7, 2)}</li>
     <li>{calc.divide(5, 2)}</li>
   </ul>
+);
+
+name.render(
+  <div>
+    <h1>Hello {fname}</h1>
+    <p>Your Luck number is {number}</p>
+  </div>,
+  document.getElementById("name")
 );
