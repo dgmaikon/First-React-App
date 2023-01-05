f163575e1c774ea486ee8ec63dba65b0542e17f0
dgmaikon <dgmaikon@hotmail.com>
2023-01-05 14:23:45 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as calc from './calculator.js';
+
+/* import {add, subtract, multiply, divide}; */ //import function by function
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <ul>
+    <li>{calc.add(1, 2)}</li>
+    <li>{calc.multiply(2, 3)}</li>
+    <li>{calc.subtract(7, 2)}</li>
+    <li>{calc.divide(5, 2)}</li>
+  </ul>
+);
